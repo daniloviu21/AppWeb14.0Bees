2a03499bc0c0171e4bdde39ea39fccb9505ab13c
jrvrr <moralesriveraj10@gmail>
2025-03-10 00:22:46 -0600

    feat: agregar tablas de ordenes y actividad

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit} from '@angular/core';
 
-
 interface Pedido {
   id: number;
   cliente: string;
@@ -49,4 +48,83 @@ export class DashboardComponent implements OnInit {
       { fecha: new Date(2024, 2, 10), cliente: 'Carlos Ramírez', estado: 'Entregado' }
     ];
   }
+
+  orders = [
+    {
+      title: 'The Speaker',
+      description: 'Nuevo pedido recibido',
+      time: 'hace 2 minutos',
+      discount: 20,
+      image: 'assets/speaker.png'
+    },
+    {
+      title: 'Head Massage',
+      description: 'Nuevo pedido recibido',
+      time: 'hace 3 minutos',
+      discount: 30,
+      image: 'assets/head-massage.png'
+    },
+    {
+      title: 'Game Box',
+      description: 'Nuevo pedido recibido',
+      time: 'hace 5 minutos',
+      discount: 60,
+      image: 'assets/game-box.png'
+    },
+    {
+      title: 'Game Box',
+      description: 'Nuevo pedido recibido',
+      time: 'hace 5 minutos',
+      discount: 60,
+      image: 'assets/game-box.png'
+    },
+    {
+      title: 'Game Box',
+      description: 'Nuevo pedido recibido',
+      time: 'hace 5 minutos',
+      discount: 60,
+      image: 'assets/game-box.png'
+    }
+
+  ];
+
+  payments = [
+    {
+      name: 'Christian Ochoa',
+      description: 'Pago pendiente. #26788',
+      time: 'hace 3 minutos',
+      avatar: 'assets/user1.png'
+    },
+    {
+      name: 'Socorro Decker',
+      description: 'Pago realizado #23457',
+      time: 'hace 7 minutos',
+      avatar: 'assets/user2.png'
+    },
+    {
+      name: 'Haywood Sharp',
+      description: '',
+      time: 'hace 16 horas',
+      avatar: 'assets/user3.png'
+    },
+    {
+      name: 'Haywood Sharp',
+      description: '',
+      time: 'hace 6 horas',
+      avatar: 'assets/user3.png'
+    },
+    {
+      name: 'Haywood Sharp',
+      description: '',
+      time: 'hace 4 horas',
+      avatar: 'assets/user3.png'
+    }
+  ];
+  transactions = [
+    { title: 'Visa Card', description: 'Laptop Ordered', amount: -1590, type: 'Card' },
+    { title: 'Paypal', description: 'Payment Received', amount: 310, type: 'Paypal' },
+    { title: 'Travel', description: 'Yosemite Trip', amount: -4900, type: 'Travel' },
+    { title: 'Shopping', description: 'Bill Paid', amount: -285, type: 'Shopping' }
+  ];
 }
+
