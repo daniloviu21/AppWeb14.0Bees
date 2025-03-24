114a3ef4ee7f1093f085b73e871b6c9ccc84208f
jrvrr <moralesriveraj10@gmail>
2025-03-23 21:34:59 -0600

    test: funcion en el usuario

--- src/app/pages/usuario-form/usuario-form.component.ts
+++ src/app/pages/usuario-form/usuario-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-form',
@@ -7,6 +10,49 @@ import { Component } from '@angular/core';
   templateUrl: './usuario-form.component.html',
   styleUrl: './usuario-form.component.css'
 })
-export class UsuarioFormComponent {
+export class UsuarioFormComponent  implements OnInit {
+  usuarioForm: FormGroup;
+  id: number | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private route: ActivatedRoute,
+    public router: Router
+  ) {
+    this.usuarioForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rol: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    if (this.id) {
+      this.usuarioService.getUsuarioById(this.id).subscribe(data => {
+        this.usuarioForm.patchValue(data);
+      });
+    }
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/usuarios']);
+  }  
+
+  guardarUsuario(): void {
+    if (this.usuarioForm.invalid) return;
+
+    if (this.id) {
+      this.usuarioService.updateUsuario(this.id, this.usuarioForm.value).subscribe(() => {
+        this.router.navigate(['/usuarios']);
+      });
+    } else {
+      this.usuarioService.createUsuario(this.usuarioForm.value).subscribe(() => {
+        this.router.navigate(['/usuarios']);
+      });
+    }
+  }
 
 }
--- src/app/pages/usuario-list/usuario-list.component.ts
+++ src/app/pages/usuario-list/usuario-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-usuario-list',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './usuario-list.component.html',
   styleUrl: './usuario-list.component.css'
 })
-export class UsuarioListComponent {
+export class UsuarioListComponent implements OnInit {
+  usuarios: any[] = [];
+router: any;
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.loadUsuarios();
+  }
+
+  loadUsuarios(): void {
+    this.usuarioService.getUsuarios().subscribe(data => {
+      this.usuarios = data;
+    });
+  }
+
+  navegarANuevoUsuario(): void {
+    this.router.navigate(['/usuarios/nuevo']);
+  }
+
+  editarUsuario(id: number): void {
+    this.router.navigate(['/usuarios/editar', id]);
+  }
+  
+
+  deleteUsuario(id: number): void {
+    if (confirm('¿Seguro que deseas eliminar este usuario?')) {
+      this.usuarioService.deleteUsuario(id).subscribe(() => {
+        this.loadUsuarios();
+      });
+    }
+  }
 
 }
